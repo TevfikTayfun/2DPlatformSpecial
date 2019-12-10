23a50a18bd362b9485318e26a070137d33de2277
TevfikTayfun <54548701+TevfikTayfun@users.noreply.github.com>
2019-12-10 17:01:42 +0300

    Controller and background scroll
    Controller bakın ordan yürüyeceğiz baya bir şeye background da şimdilik nebula koydum illaha editleriz.

---
+++ 2DPlatformer/Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour
+{
+    MeshRenderer _mr;
+    Material _mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mr = GetComponent<MeshRenderer>();
+        _mat = _mr.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = _mat.mainTextureOffset;
+        offset.x += Time.deltaTime / 10;
+        _mat.mainTextureOffset = offset;
+    }
+}
