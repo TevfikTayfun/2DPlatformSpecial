40e6c7e5b53dcdf4421979a1127dbb9dcf0e5099
TevfikTayfun <54548701+TevfikTayfun@users.noreply.github.com>
2019-12-11 15:43:52 +0300

    Movement still.

---
+++ 2DPlatformer/Assets/Scripts/BetterJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour
+{
+    public float f_fallMultiplier = 2.5f;
+    public float f_lowJumpMultiplier = 2f;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (f_fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (f_lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
